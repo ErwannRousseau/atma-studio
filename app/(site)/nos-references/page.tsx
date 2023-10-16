443602ac3472cb5813eec924852d68356df4b26c
ErwannRousseau <erwann.rousseau@icloud.com>
2023-10-16 17:42:01 +0200

    SEO metadata improvement and update some styles

--- app/(site)/nos-references/page.tsx
+++ app/(site)/nos-references/page.tsx
@@ -5,7 +5,7 @@ import HeroReferences from '@/components/HeroReferences';
 import { getReferencesPageData } from '@/sanity/lib/queries';
 
 export const metadata = {
-  title: 'Atma Studio - Nos références',
+  title: 'Nos références',
   description:
     "Explorez notre galerie d'artistes talentueux produits par ATMA Studio. Découvrez une sélection diversifiée de musiciens, chanteurs, et groupes qui ont choisi notre studio pour réaliser leurs projets musicaux. Plongez dans un univers de créativité et de musique exceptionnelle avec nos références artistiques.",
 };
