8ebb79ab87493519ae3625314380273c28db4dab
ErwannRousseau <erwann.rousseau@icloud.com>
2023-10-19 12:52:18 +0200

    update favicon to try to display it on google search

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,10 @@ export const metadata: Metadata = {
   description:
     "Découvrez ATMA Studio, studio d'enregistrement, niché au cœur de la butte Sainte-Anne à Nantes. Notre équipe d'ingénieurs du son qualifiée, excelle dans une variété de styles musicaux. Enregistrement, mixage, mastering, composition et arrangement – nous offrons un accompagnement complet pour donner vie à vos créations musicales. Explorez l'union entre l'art et la technique chez ATMA Studio.",
   icons: {
-    icon: [{ url: '/icon.png' }, new URL('/icon.png', 'https://studio-atma.com')],
+    icon: [
+      { url: 'https://studio-atma.com/favicon.ico', type: 'image/x-icon' },
+      new URL('/icon.png', 'https://studio-atma.com'),
+    ],
     shortcut: '/shortcut-icon.ico',
     apple: [{ url: '/apple-icon.png' }, { url: '/apple-icon.png', sizes: '180x180', type: 'image/png' }],
   },
